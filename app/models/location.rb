6d975b441d8ec6505e13b789e8569865511a18a9
Carlynn <chick04sd@gmail.com>
2017-09-20 15:52:43 -0700

    model association and routes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :users, through: :posts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :locations, through: :posts
 end
