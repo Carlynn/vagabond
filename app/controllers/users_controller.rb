5b379669894da3acd61ef10eb0f572f015f36859
Carlynn <chick04sd@gmail.com>
2017-09-20 18:16:52 -0700

    edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,20 @@ class UsersController < ApplicationController
   def edit
     @user = User.find_by_id(params[:id])
   end
+  def update
+    user_id = params[:id]
+    user = User.find_by_id(user_id)
+    user.update_attributes(user_update_params)
+    if user.save
+      redirect_to user_path(user)
+    end
+  end
 
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :email, :password)
+  end
+  def user_update_params
+    params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :email)
   end
 end
