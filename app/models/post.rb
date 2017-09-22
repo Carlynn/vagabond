c21b250842bbaa87c7f841d3a52389d5fb058a01
Jimmy Cheng <jccoding305@gmail.com>
2017-09-22 16:58:53 -0700

    Got character count to work correctly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :location, optional: true
+
+  validates_length_of :title, :within => 1..200, :too_long => "Title is too long!", :too_short => "Title is too short!" , allow_blank: false
 end
