d4a0e34d49f06c5072fe732bd837d15deb54f7be
raybchoi <raybchoi@gmail.com>
2017-09-24 19:56:55 -0700

    image bug fixes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,7 @@ class LocationsController < ApplicationController
   end
   def show
     @location = Location.friendly.find(params[:slug])
-    @locationposts = @location.posts.order(created_at: :desc).page(params[:page]).per(4)
+    @locationposts = @location.posts.order(created_at: :desc).page(params[:page]).per(5)
   end
 
 end
