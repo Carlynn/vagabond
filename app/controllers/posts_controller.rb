302ed0f777016d4c30777648274100b6f9757105
spragala <spragala@yahoo.com>
2017-09-25 16:17:19 -0700

    Refactored posts controller to make it more DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_login, except: [:show]
   before_action :check_owner, only: [:edit, :update, :destroy]
-
-  def index
-    redirect_to root_path
-  end
+  before_action :find_post, only: [:edit, :show, :update]
 
   def new
     @post = Post.new
@@ -16,37 +13,31 @@ class PostsController < ApplicationController
     location = params[:location][:city]
     @location = Location.find_by(city: location)
     if @post.save
-      @user.posts<<@post
-      @location.posts<<@post
+      @user.posts << @post
+      @location.posts << @post
     end
     redirect_to user_path(@user)
   end
 
   def edit
-    @post = Post.find_by_id(params[:id])
   end
 
   def update
-    post = Post.find_by_id(params[:id])
     location_params = params[:location][:city]
     @location = Location.find_by(city: location_params)
-    post.update_attributes(post_params)
+    @post.update_attributes(post_params)
     # if save - update attributes
-    if post.save
-      post.update_attributes(post_params)
-      # I THINK THAT THE LOCATION ID SHOULD NOT CHANGE ONCE IT IS CREATED (IT SHOULD BE A NON EDITABLE STATIC FIELD THOUGHTS? AND THEN WE SHOULD NOT UPDATE THE but rather figure out how to undo the push above into the location)
-      post.update_attribute(:location_id, @location.id)
-      redirect_to post_path(post)
+    if @post.save
+      @post.update_attributes(post_params)
+      @post.update_attribute(:location_id, @location.id)
+      redirect_to post_path(@post)
     else
-
-      flash[:error] = post.errors.messages[:title].join(". ")
+      flash[:error] = @post.errors.messages[:title].join(". ")
       render :edit
     end
   end
 
   def show
-    @post = Post.find(params[:id])
-    # @location = Location.find(params[:id])
   end
 
   def destroy
@@ -56,7 +47,12 @@ class PostsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
   private
+
   def post_params
     params.require(:post).permit(:title, :description, :id, :image)
   end
@@ -70,9 +66,3 @@ class PostsController < ApplicationController
     end
   end
 end
-
-# Push the post into the first user
-# User.first.posts<<Post.first
-
-# Push the post into the location to sure both parts are filled in
-# Location.first.posts <<Post.first
