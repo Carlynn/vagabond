77c4830d35152685390d55cdafb42b9d2ec34453
spragala <spragala@yahoo.com>
2017-09-25 16:00:48 -0700

    Cleaned up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,26 +5,27 @@ class UsersController < ApplicationController
   def index
     redirect_to root_path
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.create(user_params)
     login(@user)
     redirect_to user_path(@user)
   end
+
   def show
-    # @user = User.find_by_id(params[:id])
     @user = User.friendly.find(params[:slug])
-    # @posts = @user.posts
-    # used for pagination
     @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(5)
   end
+
   def edit
     @user = User.friendly.find(params[:slug])
   end
+
   def update
-    # user_id = params[:id]
     user = User.find(params[:slug])
     user.update_attributes(user_update_params)
     if user.save
@@ -54,19 +55,18 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :email, :password, :image)
   end
+
   def user_update_params
     params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :email, :image)
   end
+
   def update_password_params
     params.require(:user).permit(:password)
   end
 
   def check_owner
-  # both are the same thing current_user.id == params[:id] and session[:id] === params[:id] => only if these are true then it allows the method to work for that SPECIFIC PARAMETER IN THE URL
-  # if the person that is logged is not the same as the persons data we are looking at ... flash a message or redirect_to their own page
     if session[:user_id] != User.friendly.find(params[:slug]).id
       redirect_to user_path(current_user)
-
     end
   end
 end
